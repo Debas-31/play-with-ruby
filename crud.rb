135068fc569b6ecf17c05c74fa8f6c884493d15b
debas-31 <debas31@gmail.com>
2022-05-30 23:15:13 +0300

    Add create secure users and authenticate user method

--- crud.rb
+++ crud.rb
@@ -18,3 +18,28 @@ users = [
 def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
+
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+  ist_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+new_users = create_secure_users(users)
+puts new_users
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+      return user_record
+    end
+  end
+  puts "Credentials were not correct"
+end
+
+p authenticate_user('mashrur', 'password6', users)
