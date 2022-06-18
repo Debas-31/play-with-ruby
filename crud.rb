110ede047cccb76b2bc5e75d6dd9db2f398eb5ce
debas-31 <debas31@gmail.com>
2022-06-18 12:42:44 +0300

    Modify crud by introducing a class

--- crud.rb
+++ crud.rb
@@ -7,29 +7,27 @@ end
 
 module Crud
   require 'bcrypt'
-  puts "Module CRUD activated"
+  puts 'Module CRUD activated'
 
-def Crud.create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def Crud.verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def Crud.create_secure_users(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  list_of_users
-end
 
-def Crud.authenticate_user(username, password, list_of_users)
-  list_of_users.each do |user_record|
-    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
-      return user_record
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+
+  def create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
     end
+    list_of_users
   end
-  puts "Credentials were not correct"
-end
+
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      return user_record if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+    end
+    puts 'Credentials were not correct'
+  end
 end
