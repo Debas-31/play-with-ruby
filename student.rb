c7ebb643c16bcfa21df511ca14ab79fb6b420f48
debas-31 <debas31@gmail.com>
2022-06-18 13:27:27 +0300

    Add student class to create objects and access Crud module methods

--- student.rb
+++ student.rb
@@ -11,4 +11,17 @@ class Student
     @email = email
     @password = password
   end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address: #{@email}"
+  end
+
+  debas = Student.new('Debas', 'Ngus', 'dbus', 'debas31@gmail.com',  'password1')
+
+  jhon = Student.new('Jhon', 'Doe', 'dbus', 'example@gmail.com',  'password2')
+
+  hashed_password = debas.create_hash_digest(debas.password)
+
+  puts hashed_password
+
 end
